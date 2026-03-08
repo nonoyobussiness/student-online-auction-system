8503205542070ac14b0265e5bb9ab8abfb7e76b1
nonoyobussiness <sreevathsal2006@gmail.com>
2026-03-08 15:28:42 +0530

    done with authorization, added communication with the frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Home from "./components/Home";
 
 export default function App() {
   return (
     <Routes>
+      <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<ProtectedRoute><Home></Home></ProtectedRoute>}></Route>
     </Routes>
   );
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home(){
+    return(
+        <div className="w-screen h-screen flex items-center justify-center">
+            <h1 className="text-4xl font-bold">✨Flamboyant Home Page✨</h1>
+        </div>
+    );
+}
