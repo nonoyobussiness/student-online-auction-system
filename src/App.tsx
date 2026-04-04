1d50ae6086dc019b4eeb0b15e82b98138324f8e1
Eswar <eswarchandra.016@gmail.com>
2026-04-04 15:59:43 +0530

    feat: add CategoriesBar with theme support and light theme colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import ProtectedRoute from "./components/ProtectedRoute";
 import Home from "./pages/Home";
 import ButtonPreview from "./components/ui/ButtonPreview";
 import BadgePreview from "./components/ui/BadgePreview";
+import CategoryBarPreview from "./components/sections/CategoryBarPreview";
 
 export default function App() {
   return (
@@ -12,17 +12,10 @@ export default function App() {
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route
-        path="/home"
-        element={
-          /**<ProtectedRoute>
-            <Home />
-          </ProtectedRoute> */
-          <Home/>
-        }
-      />
+      <Route path="/home" element={<Home />} />
       <Route path="/buttons" element={<ButtonPreview />} />
       <Route path="/badges" element={<BadgePreview />} />
+      <Route path="/categorybar-preview" element={<CategoryBarPreview />} />
     </Routes>
   );
 }
